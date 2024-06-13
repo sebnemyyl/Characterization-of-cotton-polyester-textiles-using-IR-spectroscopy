bd343f5689fd566c9c569c742067e58162a88765
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-06-13 18:03:52 +0200

    Created package structure

--- src/data_prep.R
+++ src/data_prep.R
@@ -1,7 +1,7 @@
 # Function to extract pet content from sample name
-extract_pet_content <- function(sample_file_name) {
+extract_cotton_content <- function(sample_file_name) {
   splitted <- strsplit(sample_file_name, "_")
-  pet_content <- as.numeric(splitted[[1]][[2]])
+  pet_content <- as.numeric(splitted[[1]][[3]])
   pet_content
 }
 
@@ -9,10 +9,10 @@ extract_pet_content <- function(sample_file_name) {
 load_spectra <- function(file_path) {
   spectra <- read.csv(file_path, header = TRUE, row.names = 1, sep = ";", dec = ".")
   sample_file_names <- rownames(spectra)
-  sample_info <- sapply(sample_file_names, extract_pet_content)
-  spectra$pet_content <- sample_info
+  sample_info <- sapply(sample_file_names, extract_cotton_content)
+  spectra$cotton_content <- sample_info
   spectra
 }
 
 setwd(".")
-spectra <- load_spectra("spectra_textiles_mir.csv")
+spectra <- load_spectra("input/spectra_textiles_mir.csv")
