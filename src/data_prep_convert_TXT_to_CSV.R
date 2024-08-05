e18554e98e4a74fc2f633838a7f6f705900a6504
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-08-05 10:04:17 +0200

    Support feature columns

--- src/data_prep_convert_TXT_to_CSV.R
+++ src/data_prep_convert_TXT_to_CSV.R
@@ -12,75 +12,57 @@
 #Second step
 ####load Import Function
 
+import <- function(data, Location) {
+  path <- file.path(Location, data)
+  df <- try(read.csv(path, header = FALSE,sep = ",", dec = "."))
+  return(df)
+}
+
+split_file_name <- function(file_path) {
+  file_name_with_ending <- basename((file_path))
+  file_name <- strsplit(file_name_with_ending, split = "\\.")[[1]][1]
+  feature_list <- strsplit(file_name, split = "_")[[1]]
+  return(feature_list)
+}
+
+extract_number <- function(feature_list, index) {
+  text <- sapply(feature_list, `[[`, index)
+  number <- regmatches(text, regexpr("[0-9]+", text))
+  return(number)
+} 
+# TODO rename location to folder something
+
 #########################
 ##Import Function
 #########################
 # Location: Path, where the TXT.files are located
-# Gruppen: string vector of the used groups, must be part of file name and be explicit
-# A maximum of 8 groups is possible based on the available colours
-
-My.Import <- function(Location, Gruppen = Files) {
+My.Import <- function(Location) {
   # List of the TXT.files in location
-  Files <- list.files(path = Location, recursive=TRUE, include.dirs=TRUE, pattern = ".txt")
-  Files1 <- c()
-  print(Files)
-  for (x in 1:length(Gruppen)) {
-    Files1 <- c(Files1,Files[grep(pattern = as.character(Gruppen[x]), x = Files)])
-  }
-  
-  import <- function(data) {
-    start <- getwd() ##store Start wd
-    setwd(Location) ### setwd to Data file
-    
-    df <- try(read.csv(data, header = FALSE, 
-                       sep = ",",              
-                       dec = "." ))         
-    setwd(start) ## back to normal
-    return(df)  ### Output = df
-  }
-
+  Files1 <- list.files(path = Location, recursive = TRUE, include.dirs = TRUE, pattern = ".txt")
   # Import of the files
-  Raw.list <- lapply(Files1,import)
-  
+  raw_dfs <- lapply(Files1, import, Location = Location)
   # combine list of data frames 
-  Raw.data <- do.call("cbind", Raw.list)
+  Raw.data <- do.call("cbind", raw_dfs)
   
+
   # Remove wavenumbers
-  Spectra <- as.data.frame(t(Raw.data[,-c(which(colnames(Raw.data) == "V1"))]))
+  Spectra <- as.data.frame(t(Raw.data[, -c(which(colnames(Raw.data) == "V1"))]))
   
   # Extract wavenumbers and set as column names
   Wavenumber <- round(as.numeric(Raw.data[,1]),2)
   colnames(Spectra) <- Wavenumber
-
-  ### Test for a Gruppen vector, else the files are organised according to files vector
-  if(Gruppen[1] != Files1[1]){
-    ##### construct Groups vector
-    Groups <- c(1:length(Files1))
-    for (i in 1:length(Gruppen)){
-      Pos <- c(grep(pattern = as.character(Gruppen[i]), x = Files1)) ##grep recognizes patterns in file names
-      Groups[Pos] <- Gruppen[i] # appends vector
-    }
-  }else{
-    Groups <- sub(pattern = ".TXT",x= Gruppen, replacement = "")
-  }
   
   rownames(Spectra) <- Files1
-
-  ##### Creation of a list containig spectra, wavenumbers and Groups 
-  ###Zur Besseren Organisation
-  OriginalData <- list(Wavenumber,Spectra,Files1)
-  
-  names(OriginalData) <- c("Wavenumber","Spectra","Filenames")
-  
-  # Output = list
-  return(OriginalData)
+  features <- lapply(Files1, split_file_name)
+  Spectra$pet <- sapply(features, `[[`, 2) #TODO could be cotton instead
+  Spectra$cotton <- sapply(features, `[[`, 3)
+  Spectra$specimen <- extract_number(features, 4)
+  Spectra$area <- extract_number(features, 5)
+  Spectra$spot <- extract_number(features, 6)
+  Spectra$measuring_date <- sapply(features, `[[`, 7)
+  return(Spectra)
 }
 
-#Third Step
-#to save spectra take part of spectra name
-data_raw <- My.Import(Location = "C:/Users/sebne/Documents/FHWN_Tulln/DataAnalysis/repo/input/clean_txt/NIR")
-#data <- cbind(data_raw$Filenames,data_raw$Spectra)
-#colnames(data)[names(data) == "data_raw$Filenames"] <- ""
+spectra <- My.Import(Location = "input/clean_txt/NIR")
 #write to csv file
-write.csv2(data_raw$Spectra,"spectra_nir_240523.csv", row.names = TRUE)
-
+write.csv2(spectra, "spectra_nir_240523.csv", row.names = TRUE)
