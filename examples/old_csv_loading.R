1a6833a9dac36cf97d1093ee564c0b7c1dba3ed2
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-08-13 11:49:25 +0200

    Added PLS function and extracted old code for testing

---
+++ examples/old_csv_loading.R
@@ -0,0 +1,65 @@
+
+library(pls)
+library(plotly)
+library(dplyr)
+library(prospectr)
+
+
+setwd(".")
+Data_Raw <- read.csv("input/spectra_mir_240626.csv",header=TRUE,sep=";",dec=",", row.names = 1)
+
+
+### assign variables
+Spectra_Original <- Data_Raw[,-c(1:2)]
+Probenbezeichnung <- rownames(Data_Raw)
+
+# Function to extract the first number from a string
+extract_number <- function(string) {
+  number <- as.numeric(sub("\\D*(\\d+).*", "\\1", string))
+  ifelse(is.na(number), 50, number)
+}
+
+# Extracting numbers from sample names
+numbers <- sapply(Probenbezeichnung, extract_number)
+numbers[numbers < 10] <- 50
+
+Reference_000 <- numbers
+###Wellenzahl erzeugen
+Wellenzahl_000 <- as.numeric(sub("X","0",names(Spectra_Original)))
+names(Spectra_Original) <- as.numeric(sub("X","0",names(Spectra_Original)))
+
+#Spektren als Matrix abspeichern
+Spectra_000 <- data.matrix(Spectra_Original) 
+#Referenzwerte definieren
+Reference_000 <- Reference_000
+#Dataframe erstellen
+Data_000 <- data.frame(Reference=I(Reference_000),Spektren=(Spectra_000))
+
+# SNV
+Spectra_100 <- standardNormalVariate(Spectra_Original)
+Reference_100 <- 100-Reference_000
+# Outlier Removal
+# 4th sample is the outlier for both here and mdatools 
+
+Spectra_101 <- Spectra_100
+Spectra_101 <- Spectra_101[-c(4),]
+
+Reference_101 <- Reference_100
+Reference_101 <- Reference_101[-c(4)]
+
+#Spektralen Bereich einschraenken
+Spectra_101[,c(1:200,610:1140,1715:1760)] <- 0
+Wellenzahl_101 <- Wellenzahl_000
+
+#Spektren als Matrix abspeichern
+Spectra_101 <- data.matrix(Spectra_101) 
+#Dataframe erstellen
+Data_101 <- data.frame(Reference=I(Reference_101),Spektren=(Spectra_101))
+
+
+#matplot(Wellenzahl_101,t(Spectra_101),lty=1,type="l",ylab="Absorbance")
+
+options(digits = 4)
+pls.options(plsalg="oscorespls")
+PLS_101 <- plsr(Reference_101 ~ Spectra_101, data=Data_101, ncomp=10, val="LOO")
+summary(PLS_101)
--- src/data_prep.R
+++ src/data_prep.R
@@ -6,6 +6,7 @@ library(prospectr)
 load_csv <- function(csv_path) {
   # Load CSV file
   csv_raw <- read.csv(csv_path, header = TRUE, sep = ";", dec = ",", row.names = 1)
+  # TODO maybe we have to remove two first columns like in original
   # Extract spectra and features
   spectra_raw <- csv_raw[, -c(1763:1768)]
   features <- csv_raw[, c(1763:1768)]
@@ -23,8 +24,8 @@ clean_up_spectra <- function(spectra_df) {
   spectra <- spectra_df %>% select(starts_with("spectra"))
   # SNV
   spectra <- standardNormalVariate(spectra)
-  # Outlier Removal
-  # 4th sample is the outlier for both here and mdatools (???)
+  # Outlier Removal (4th sample is the outlier for here and mdatools)
+  # TODO check what this 4th sample means..
   spectra <- spectra[-c(4), ]
   reference <- reference[-c(4), ]
   # Spektralen Bereich einschraenken
@@ -34,9 +35,23 @@ clean_up_spectra <- function(spectra_df) {
   return(clean_df)
 }
 
+run_pls <- function(spectra_df) {
+  options(digits = 4)
+  pls.options(plsalg = "oscorespls")
+  ref <- spectra_df$reference.cotton
+  spectra <- spectra_df_clean %>% select(starts_with("spectra"))
+  spectra_matrix <- data.matrix(spectra)
+  pls_result <- plsr(ref ~ spectra_matrix, data = spectra_df, ncomp = 10, val = "LOO")
+  summary(pls_result)
+}
+
 setwd(".")
 csv_path <- "input/spectra_mir_240806.csv"
 spectra_df_full <- load_csv(csv_path)
 spectra_df_clean <- clean_up_spectra(spectra_df_full)
-print(spectra_df_clean[20])
-print(spectra_df_full[20])
+#print(spectra_df_clean[20])
+#print(spectra_df_full[20])
+run_pls(spectra_df_clean)
+
+# TODO doesnt work well (but in original it is also bad)
+#matplot(names(spectra), t(spectra), lty = 1, type = "l", ylab = "Absorbance")
