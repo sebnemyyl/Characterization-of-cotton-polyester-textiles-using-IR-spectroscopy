22f3aab9e9ede37f1629cbf9c60f2ee7f255a03b
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-08-15 12:16:10 +0200

    Improved data prep code and created plot function

--- src/data_prep.R
+++ src/data_prep.R
@@ -6,7 +6,6 @@ library(prospectr)
 load_csv <- function(csv_path) {
   # Load CSV file
   csv_raw <- read.csv(csv_path, header = TRUE, sep = ";", dec = ",", row.names = 1)
-  # TODO maybe we have to remove two first columns like in original
   # Extract spectra and features
   spectra_raw <- csv_raw[, -c(1763:1768)]
   features <- csv_raw[, c(1763:1768)]
@@ -23,13 +22,14 @@ clean_up_spectra <- function(spectra_df) {
   reference <- spectra_df %>% select(starts_with("reference"))
   spectra <- spectra_df %>% select(starts_with("spectra"))
   # SNV
+  # TODO make SNV configurable
   spectra <- standardNormalVariate(spectra)
   # Outlier Removal (4th sample is the outlier for here and mdatools)
-  # TODO check what this 4th sample means..
+  # TODO check what this 4th sample means.. -> Extract function and make it configurable
   spectra <- spectra[-c(4), ]
   reference <- reference[-c(4), ]
-  # Spektralen Bereich einschraenken
-  spectra[, c(1:200, 610:1140, 1715:1760)] <- 0
+  # Limit spectral area
+  spectra[, c(1:200, 610:1140, 1715:1762)] <- 0
   # Recreate data frame
   clean_df <- data.frame(reference, spectra)
   return(clean_df)
@@ -38,20 +38,22 @@ clean_up_spectra <- function(spectra_df) {
 run_pls <- function(spectra_df) {
   options(digits = 4)
   pls.options(plsalg = "oscorespls")
-  ref <- spectra_df$reference.cotton
+  ref_cotton <- spectra_df$reference.cotton
   spectra <- spectra_df_clean %>% select(starts_with("spectra"))
   spectra_matrix <- data.matrix(spectra)
-  pls_result <- plsr(ref ~ spectra_matrix, data = spectra_df, ncomp = 10, val = "LOO")
+  pls_result <- plsr(ref_cotton ~ spectra_matrix, data = spectra_df, ncomp = 10, val = "LOO")
   summary(pls_result)
+}
+
+plot_spectra <- function(spectra_df) {
+  spectra <- spectra_df %>% select(starts_with("spectra"))
+  wave_numbers <- as.integer(sub("spectra.", "", names(spectra)))
+  matplot(wave_numbers, t(spectra), lty = 1, type = "l", ylab = "Absorbance")
 }
 
 setwd(".")
 csv_path <- "input/spectra_mir_240806.csv"
 spectra_df_full <- load_csv(csv_path)
 spectra_df_clean <- clean_up_spectra(spectra_df_full)
-#print(spectra_df_clean[20])
-#print(spectra_df_full[20])
-run_pls(spectra_df_clean)
-
-# TODO doesnt work well (but in original it is also bad)
-#matplot(names(spectra), t(spectra), lty = 1, type = "l", ylab = "Absorbance")
+#run_pls(spectra_df_clean)
+#plot_spectra(spectra_df_clean)
