df114731e72d65a4acf7e59baa135c6ee07c52f3
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-10-11 11:08:46 +0200

    Make limit spectra method configurable

--- src/util/03_data_prep_limit_spectra.R
+++ src/util/03_data_prep_limit_spectra.R
@@ -1,15 +1,23 @@
 library(dplyr)
 
-clean_up_spectra <- function(spectra_df) {
+clean_up_spectra <- function(spectra_df, type = "nir") {
   reference <- spectra_df %>% dplyr::select(starts_with("reference"))
   spectra <- spectra_df %>% dplyr::select(starts_with("spectra"))
   # TODO check what this 4th sample means.. -> Extract function and make it configurable
   spectra <- spectra[-c(4), ]
   reference <- reference[-c(4), ]
+  wavenumbers <- as.numeric(sub("spectra\\.", "", colnames(spectra)))
   # Limit spectral area
-  # TODO make it configurable for NIR
-  spectra[, c(1:200, 610:1140, 1715:1762)] <- 0
+  # TODO make it configurable for water band
+  if (type == "nir") {
+    columns_to_keep <- (wavenumbers >= 3787.81 & wavenumbers <= 7876.49)
+  } else if (type == "mir") {
+    columns_to_keep <- (wavenumbers >= 1801.44478 & wavenumbers <= 2798.60426)
+  } else {
+    stop("This type is not supported")
+  }
+  filtered_spectra <- spectra[, columns_to_keep]
   # Recreate data frame
-  clean_df <- data.frame(reference, spectra)
+  clean_df <- data.frame(reference, filtered_spectra)
   return(clean_df)
 }
