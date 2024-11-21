383e1dd6527306818013b0a74c904bf5d3758362
sebnem yayla <sebnem.sara.yayla@gmail.com>
2024-11-21 10:43:15 +0100

    spectra plotting function is modified and file name change

--- src/scripts/clean_up_spectra.R
+++ src/scripts/clean_up_spectra.R
@@ -5,13 +5,25 @@ library(stringr)
 source("src/util/02_data_prep_load_CSV.R")
 source("src/util/03_data_prep_limit_spectra.R")
 source("src/util/05_data_analysis_plot_spectra.R")
+source("src/util/04_data_prep_baseline_correction.R")
 
 input_path <- "input/raw_csv/spectra_nir_241011.csv"
-output_path <- "input/clean_csv/spectra_nir_resampling.csv"
+output_path <- "input/clean_csv/spectra_nir_50_snv_resampling.csv"
 type <- "nir"
 
 spectra_df_full <- load_csv(input_path)
 spectra_df_clean <- clean_up_spectra(spectra_df_full, type, remove_waterband = FALSE)
 ## Filter as needed (cotton content, date etc)
-spectra_df_clean <- spectra_df_clean %>% filter((reference.measuring_date == 240807)|(reference.measuring_date == 240812)) # NIR resampling
-save_csv(spectra_df_clean, output_path)
+spectra_df_clean <- spectra_df_clean %>% filter(reference.measuring_date == 240807 | reference.measuring_date == 240812 ) # NIR resampling
+
+spectra_df_clean$reference.pet<-as.numeric(spectra_df_clean$reference.pet)
+spectra_df_clean$reference.cotton<-as.numeric(spectra_df_clean$reference.cotton)
+
+baseline_correction_df_nir <- stdnormalvariate(spectra_df_clean)
+baseline_correction_df_detrend <- detrend(baseline_correction_df_nir,2)
+plot_spectra(baseline_correction_df_nir)
+plot_spectra(baseline_correction_df_detrend)
+
+
+
+save_csv(baseline_correction_df_nir, output_path)
--- src/util/05_data_analysis_plot_spectra.R
+++ src/util/05_data_analysis_plot_spectra.R
@@ -1,8 +1,19 @@
 library(pls)
 library(plotly)
+library(dplyr)
 
 plot_spectra <- function(spectra_df) {
   spectra <- spectra_df %>% dplyr::select(starts_with("spectra"))
+  
+  # Reverse wave numbers (decreasing order of wavenumbers)
   wave_numbers <- as.integer(sub("spectra.", "", names(spectra)))
-  matplot(wave_numbers, t(spectra), lty = 1, type = "l", ylab = "Absorbance")
+  wave_numbers <- rev(wave_numbers)
+  
+  # Reverse the spectra matrix to match the reversed wave numbers
+  spectra <- spectra[, rev(seq_along(wave_numbers))]
+  
+  # Specify xlim so that x axis is also reversed
+  matplot(wave_numbers, t(spectra), lty = 1, type = "l", xlim = c(8000, 4000),
+          ylab = "Absorbance (%)", xlab = "Wavenumber (1/cm)")
 }
+
